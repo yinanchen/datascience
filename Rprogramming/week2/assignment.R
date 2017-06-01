95dac318bcd064cb4fa29bff13958ee8ecb76f86
Julia Chen <ya.chen1212@gmail.com>
2017-06-01 14:37:57 +0800

    Update assignment.R

--- Rprogramming/week2/assignment.R
+++ Rprogramming/week2/assignment.R
@@ -23,11 +23,11 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
 
 
 #sulfate, nitrate
-#x <- pollutantmean("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/R programming/rprog%2Fdata%2Fspecdata/specdata","nitrate")
+#x <- pollutantmean("directory","nitrate")
 #print(x)
 
 # Other ref
-#setwd("C:/Users/americo/Documents/Documenti_2_0/Education/Data science/Coursera/R programming/rworkingdir/specdata")
+#setwd("directory")
 #lista_tot1 = list.files(pattern="*.csv");
 #lista_tot2 = lapply(lista_tot1, read.csv, sep = ",")#stampa tutti i csv                                  
 #lista_tot3<-do.call("rbind", lista_tot2)
@@ -53,7 +53,7 @@ complete <- function(directory, id = 1:332){
 }
 
 set.seed(42)
-#cc <- complete("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/R programming/rprog%2Fdata%2Fspecdata/specdata", 332:1)
+#cc <- complete("directory", 332:1)
 #use <- sample(332, 10)
 #print(cc[use, "nobs"])
 
@@ -81,22 +81,22 @@ corr <- function(directory, threshold = 0){
 	return(corr_vector)
 }
 
-#x <- corr("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/R programming/rprog%2Fdata%2Fspecdata/specdata", 5000)
-#cr <- corr("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/R programming/rprog%2Fdata%2Fspecdata/specdata")                
+#x <- corr("directory", 5000)
+#cr <- corr("directory")                
 #cr <- sort(cr)                
 #set.seed(868)                
 #out <- round(cr[sample(length(cr), 5)], 4)
 #print(out)
 
-#cr <- corr("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/R programming/rprog%2Fdata%2Fspecdata/specdata", 129)                
+#cr <- corr("directory", 129)                
 #cr <- sort(cr)                
 #n <- length(cr)                
 #set.seed(197)                
 #out <- c(n, round(cr[sample(n, 5)], 4))
 #print(out)
 
-cr <- corr("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/R programming/rprog%2Fdata%2Fspecdata/specdata", 2000)                
+cr <- corr("directory", 2000)                
 n <- length(cr)                
-cr <- corr("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/R programming/rprog%2Fdata%2Fspecdata/specdata", 1000)                
+cr <- corr("directory", 1000)                
 cr <- sort(cr)
 print(c(n, round(cr, 4)))
