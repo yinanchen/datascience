1b6e27458bc244a8ca8ecbdb42a34db843ca1d41
Julia Chen <ya.chen1212@gmail.com>
2017-06-12 18:50:00 +0800

    Create plot1.R

---
+++ ExDataAnalysis/plot1.R
@@ -0,0 +1,15 @@
+# TODO: Add comment
+# Author: yachenq
+###############################################################################
+dataPath <- "D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week1/household_power_consumption.txt"
+pngPath <- "D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week1/plot1.png"
+
+data <- read.csv(dataPath, sep = ";")
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data <- subset(data, data$Date >= "2007-02-01" & data$Date <= "2007-02-02")
+dateTime <- paste(data$Date, data$Time)
+data$Datetime <- as.POSIXct(dateTime)
+
+hist(as.numeric(as.character(data$Global_active_power)),  main="Global Active Power", col = "red", xlab = "Global active power (kilowatts)")
+dev.copy(png, file=pngPath, height=480, width=480)
+dev.off ()
