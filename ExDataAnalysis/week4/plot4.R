cf2be3165402f28beccc866d4e44dab832ccc13c
Julia Chen <ya.chen1212@gmail.com>
2017-06-22 15:03:05 +0800

    Create plot4.R

---
+++ ExDataAnalysis/week4/plot4.R
@@ -0,0 +1,21 @@
+# TODO: Add comment
+# 
+# Author: yachenq
+###############################################################################
+
+# Read data
+scc_summary_data <- readRDS("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week4/NEI_data/summarySCC_PM25.rds")
+scc_data <- readRDS("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week4/NEI_data/Source_Classification_Code.rds")
+pngPath <- "D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week4/plot4.png"
+
+# Subset and merge two dataset
+coalScc <- scc_data[grepl('Coal', scc_data$Short.Name, fixed=T), ]
+merge_data <- merge(x=scc_summary_data, y=coalScc, by="SCC")
+
+# Aggregation
+aggCoalEmi <- aggregate(Emissions ~ year, merge_data, sum)
+
+ggplot(data = aggCoalEmi, aes(x=year, y=Emissions)) + geom_line() + geom_text(aes(label=Emissions), vjust=1)+ geom_point( size=4, shape=21, fill="white") + xlab("Year") + ylab("Emissions (tons)" ) + ggtitle("Total United States PM2.5 Coal Emissions")
+
+dev.copy(png, filename=pngPath)
+dev.off ()
