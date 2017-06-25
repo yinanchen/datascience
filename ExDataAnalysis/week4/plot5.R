a31702fd4efb9df567b684af17a3907cf0c4b2a7
Julia Chen <ya.chen1212@gmail.com>
2017-06-25 22:11:37 +0800

    Update plot5.R

--- ExDataAnalysis/week4/plot5.R
+++ ExDataAnalysis/week4/plot5.R
@@ -6,17 +6,16 @@ library(plyr)
 library(ggplot2)
 
 # Read data
-EMI <- readRDS("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week4/NEI_data/summarySCC_PM25.rds")
-SCC <- readRDS("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week4/NEI_data/Source_Classification_Code.rds")
-pngPath <- "D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week4/plot5.png"
+scc_summary_data <- readRDS("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week4/NEI_data/summarySCC_PM25.rds")
+scc_data <- readRDS("D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week4/NEI_data/Source_Classification_Code.rds")
+pngPath <- "D:/Users/yachenq/Documents/ML/Course Training/Data Science Program/Exploratory Data Analysis/project/week4/plot4.png"
 
-#subset_data <- EMI[,c('SCC','year','fips','Emissions')]
-motorSCC <- SCC[grepl('vehicle', SCC$SCC.Level.Two, ignore.case=T), ]
-motorEMI <- merge(x=EMI, y=motorSCC, by="SCC")
-motorBalEMI <- subset(motorEMI, motorEMI$fips=="24510")
+motor_scc <- scc_data[grepl('vehicle', scc_data$scc_data.Level.Two, ignore.case=T), ]
+merge_data <- merge(x=scc_summary_data, y=motor_scc, by="SCC")
+motor_bal_emi <- subset(merge_data, merge_data$fips=="24510")
 
-aggMotorBalEMI <- aggregate(Emissions ~ year, motorBalEMI, sum)
+aggMotor <- aggregate(Emissions ~ year, motor_bal_emi, sum)
 
-ggplot(data = aggMotorBalEMI, aes(x=year, y=Emissions)) + geom_line() + geom_text(aes(label=Emissions), vjust=-2)+ geom_point( size=4, shape=21, fill="white") + xlab("Year") + ylab("Emissions (thousands of tons)" ) + ggtitle("Baltimore Emissions from Motorcycle")
+ggplot(data = aggMotor, aes(x=year, y=Emissions)) + geom_line() + geom_text(aes(label=Emissions), vjust=-2)+ geom_point( size=4, shape=21, fill="white") + xlab("Year") + ylab("Emissions (thousands of tons)" ) + ggtitle("Baltimore Emissions from Motorcycle")
 dev.copy(png, filename=pngPath)
 dev.off ()
